784c781d4cfef4ba72a559b61f0758d221d4067d
Ethan Frank <efrank801@gmail.com>
2022-10-25 20:48:41 -0400

    Added redirect and url_for imports with template functions.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def dashboard():
     return render_template('index.html')
+
+
+@app.route('/<name>')
+def user(name):
+    return f"Hello {name}!"
+
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for("user", name="Admin"))
 
 
 if __name__ == '__main__':
