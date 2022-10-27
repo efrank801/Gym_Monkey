e00784f749acb7fb8a0f117371fc260548491627
Ethan Frank <efrank801@gmail.com>
2022-10-26 21:32:41 -0400

    Added login page with POST and GET methods to backend.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,9 @@ def dashboard():
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
     return render_template('login.html')
 
 
@@ -22,9 +25,9 @@ def login():
 #         return render_template('login.html')
 
 
-# @app.route("/<usr>")
-# def user(usr):
-#     return f"<h1>{usr}</h1>"
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 
 if __name__ == '__main__':
