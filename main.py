0c348566364af0dae6e8ed14a1187d4f759ecbef
Ethan Frank <efrank801@gmail.com>
2022-10-27 16:25:02 -0400

    Added incorrect login error and session support.

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "secretkey"
+app.permanent_session_lifetime = timedelta(days=1)
 
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=["POST", "GET"])
 def dashboard():
-    return render_template('dashboard.html')
+    if "username" in session:
+        if "password" in session:
+            username = session["username"]
+            password = session["password"]
+            return render_template('dashboard.html')
+    else:
+        return redirect(url_for("login"))
 
 
 @app.route('/', methods=["POST", "GET"])
 def login():
+    error = None
     if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-    return render_template('login/login.html')
-
-
-# @app.route('/login', methods=["POST", "GET"])
-# def login():
-#     if request.method == "POST":
-#         usr = request.form["nm"]
-#         return redirect(url_for("user", usr=usr))
-#     else:
-#         return render_template('login.html')
+        if request.form['username'] == 'admin' or request.form['password'] == 'password':
+            if request.form.getlist('rememberme').__contains__('checked'):
+                session.permanent = True
+                print("Setting permanent session to 1 day(s).")
+            username = request.form['username']
+            password = request.form['password']
+            session['username'] = username
+            session['password'] = password
+            return redirect(url_for('dashboard'))
+        else:
+            error = 'Invalid login. Please try again.'
+    return render_template('login/login.html', error=error)
 
 
 # @app.route("/<usr>")
