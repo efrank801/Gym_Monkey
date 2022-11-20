7173e8f931f3d18f59486038cc1efe9022cea8c1
Ethan Frank <efrank801@gmail.com>
2022-11-19 21:53:15 -0500

    Created reference python.txt | Cleared main.py

--- main.py
+++ main.py
@@ -1,77 +1,17 @@
 # Imports
+from flask import Flask, render_template
 
-from datetime import timedelta
-
-from flask import Flask, render_template, redirect, url_for, request, session
-
-from flask_sqlalchemy import SQLAlchemy
 
 # Starting Flask Application
-
 app = Flask(__name__)
 app.secret_key = "secretkey"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.permanent_session_lifetime = timedelta(days=1)
 
-# Initiating SQL Database
-
-db = SQLAlchemy(app)
-
-
-class users(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-
-    def __init__(self, username, password, name):
-        self.username = username
-        self.password = password
-        self.name = name
-
-
-# Dashboard Route
-
-@app.route('/dashboard', methods=["POST", "GET"])
-def dashboard():
-    # email = None
-    if "username" in session:
-        if "password" in session:
-            # username = session["username"]
-            # password = session["password"]
-            return render_template('dashboard.html')
-    else:
-        return redirect(url_for("login"))
-
-
-# Login Route / Default Route
 
 @app.route('/', methods=["POST", "GET"])
 def login():
-    error = None
-    if request.method == "POST":
-        if request.form['username'] == 'admin' or request.form['password'] == 'password':
-            if request.form.getlist('rememberme').__contains__('checked'):
-                session.permanent = True
-                print("Setting permanent session to 1 day(s).")
-            username = request.form['username']
-            password = request.form['password']
-            session['username'] = username
-            session['password'] = password
-            return redirect(url_for('dashboard'))
-        else:
-            error = 'Invalid login. Please try again.'
-    return render_template('login.html', error=error)
+    render_template('login.html')
 
-
-# @app.route("/<usr>")
-# def user(usr):
-#     return f"<h1>{usr}</h1>"
-
-# session.pop("username", "password", None)
 
 # Run
-
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
